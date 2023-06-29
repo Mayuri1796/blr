29614f3701c58a22e3654dfac732f390718bddf5
Mayuri1796 <mayuriyeole32@gmail.com>
2023-06-29 10:14:52 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,32 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+// number count for stats, using jQuery animate
+
+$(".counting").each(function () {
+  var $this = $(this),
+    countTo = $this.attr("data-count");
+
+  $({ countNum: $this.text() }).animate(
+    {
+      countNum: countTo,
+    },
+
+    {
+      duration: 3000,
+      easing: "linear",
+      step: function () {
+        $this.text(Math.floor(this.countNum));
+      },
+      complete: function () {
+        $this.text(this.countNum);
+        //alert('finished');
+      },
+    }
+  );
+});
